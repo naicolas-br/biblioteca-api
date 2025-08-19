d02d4d3de9d5a16742588d38e5d1d3f79ffeb46d
Nicolas <nicolas.viana.moc@gmail.com>
2025-08-19 18:19:36 -0300

    fix(routes): corrigir rota de livros de um autor específico

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,7 @@
 <?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 
@@ -9,25 +12,4 @@ Route::apiResource('authors', AuthorController::class);
 Route::apiResource('books', BookController::class);
 
 // Rota adicional: livros de um autor específico
-Route::get('authors/{author}/books', [BookController::class, 'byAuthor']);
-
-// TODO: Implementar suas rotas aqui
-
-/*
- * 📋 Rotas esperadas após implementação:
- * 
- * Autores:
- * GET    /api/authors          - Listar autores (index)
- * POST   /api/authors          - Criar autor (store) 
- * GET    /api/authors/{id}     - Buscar autor (show)
- * PUT    /api/authors/{id}     - Atualizar autor (update)
- * DELETE /api/authors/{id}     - Excluir autor (destroy)
- * GET    /api/authors/{id}/books - Livros do autor (books)
- * 
- * Livros:
- * GET    /api/books            - Listar livros (index)
- * POST   /api/books            - Criar livro (store)
- * GET    /api/books/{id}       - Buscar livro (show) 
- * PUT    /api/books/{id}       - Atualizar livro (update)
- * DELETE /api/books/{id}       - Excluir livro (destroy)
- */
+Route::get('authors/{author}/books', [AuthorController::class, 'books']);
